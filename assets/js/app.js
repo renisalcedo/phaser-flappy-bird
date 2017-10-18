10b38c785dea072c27e3117e90df363e158b8f22
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-10-18 00:32:26 -0400

    Settup setting and updated instructions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,29 @@
 // Initial setup for the game
-const Game = new Phaser.Game(640, 360, Phaser.AUTO);
+const Game = new Phaser.Game(840, 600, Phaser.AUTO);
 
 let GameState = {
   // Loads the game assets before the game starts
   preload: function() {
-    this.load.image('bird', '../images/bird.png');
-    this.load.image('background', '../images/bg.png');
-    this.load.image('ground', '../images/ground.png');
-    this.load.image('tube1', '../images/tube1.png');
-    this.load.image('tube2', '../images/tube2.png');
+    this.load.image('bird', 'assets/images/bird.png');
+    this.load.image('background', 'assets/images/bg.png');
+    this.load.image('ground', 'assets/images/ground.png');
+    this.load.image('tube1', 'assets/images/tube1.png');
+    this.load.image('tube2', 'assets/images/tube2.png');
   },
 
   create: function() {
+    // Adds Responsiveness to the game
+    // SHOW_ALL makes the game fit the screen but keeps aspect ratio
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
+    // Adds background to game
+    this.background = this.game.add.sprite(0, 0, 'background');
+    this.background.scale.setTo(3, 1.4);
+
+    // Adds ground below background to game
+    this.ground = this.game.add.sprite(0, 500, 'ground');
+    this.ground.scale.setTo(3, 1);
 
   },
 
@@ -19,3 +31,7 @@ let GameState = {
 
   }
 };
+
+
+Game.state.add('GameState', GameState);
+Game.state.start('GameState');
