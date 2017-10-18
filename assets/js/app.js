87ea7f6fd732cfd69ea9de2e7f5758a69ef39fad
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-10-18 01:26:13 -0400

    Added physics, gravity and collision

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // Initial setup for the game
 const Game = new Phaser.Game(840, 600, Phaser.AUTO);
 
-let GameState = {
+const GameState = {
   // Loads the game assets before the game starts
   preload: function() {
     this.load.image('bird', 'assets/images/bird.png');
@@ -12,6 +12,7 @@ let GameState = {
   },
 
   create: function() {
+    const gravityVal = 500;
     // Adds Responsiveness to the game
     // SHOW_ALL makes the game fit the screen but keeps aspect ratio
     this.scale.pageAlignHorizontally = true;
@@ -25,10 +26,33 @@ let GameState = {
     this.ground = this.game.add.sprite(0, 500, 'ground');
     this.ground.scale.setTo(3, 1);
 
+    // The bird in the game
+    this.bird = this.game.add.sprite(Game.height-400, Game.height/2, 'bird')
+
+    this.startPhysics([this.bird, this.ground]);
+
+    // Activates collision and gravity for bird
+    this.bird.body.collideWorldBounds = true;
+    this.bird.body.gravity.y = gravityVal;
+
+    // Activates collision for ground
+    this.ground.body.collideWorldBounds = true;
+    this.ground.checkCollision.up = false;
+    this.ground.checkCollision.down = false;
+    this.ground.body.immovable = true;
   },
 
   update: function() {
+    Game.physics.arcade.collide(this.bird, this.ground);
+  },
 
+
+  // Adds Physics and Gravity to game
+  startPhysics: function(el) {
+    // Starts the physics on game and sets value for gravity
+    Game.physics.startSystem(Phaser.Physics.ARCADE);
+    // Enables physics on element on game
+    Game.physics.enable(el, Phaser.Physics.ARCADE);
   }
 };
 
